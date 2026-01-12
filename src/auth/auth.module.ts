ce19cf1752eba5bb82f65036149a62e97fdbe88a
khanh <anhkietnguyenkhanh@icloud.com>
2026-01-12 10:59:58 -0500

    login with jwt access token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,22 @@
+
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      // secret: process.env.JWT_ACCESS_SECRET_KEY,
+      secret: `${process.env.JWT_SECRET}`,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
+  exports: [AuthService],
 })
 export class AuthModule {}
