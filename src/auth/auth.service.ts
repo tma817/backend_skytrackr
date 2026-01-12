f43e54fe96ef77604973deb6c448ed77190dcd38
Arky Lun <arkylun@gmail.com>
2026-01-12 13:18:17 -0500

    implement register feature

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,21 +4,31 @@ import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(
-        private usersService: UsersService,
-        private jwtService: JwtService
-    ) {}
-    
-    async signIn(username: string, pass: string,): Promise<{ access_token: string }> 
-    {
-        const user = await this.usersService.findOne(username);
-        if (user?.password !== pass) {
-            throw new UnauthorizedException();
-        }
-        const payload = { sub: user.userId, username: user.username };
-        console.log(payload)
-        return {
-            access_token: await this.jwtService.signAsync(payload),
-        };
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async signIn(
+    username: string,
+    pass: string,
+  ): Promise<{ access_token: string }> {
+    const user = await this.usersService.findOne(username);
+    if (user?.password !== pass) {
+      throw new UnauthorizedException();
     }
+    const payload = { sub: user.userId, username: user.username };
+    console.log(payload);
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
+  }
+
+  async register(
+    username: string,
+    password: string,
+  ): Promise<{ userId: number; username: string }> {
+    const user = await this.usersService.create({ username, password });
+    return { userId: user.userId, username: user.username };
+  }
 }
