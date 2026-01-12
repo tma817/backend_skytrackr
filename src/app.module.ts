baf2c16a7a05c881948153c466e2988737563dd4
Arky Lun <arkylun@gmail.com>
2026-01-12 12:39:22 -0500

    Revert "Merge pull request #2 from tma817/khanh/login-feature"
    This reverts commit 20094f916da43eec9904abbaadb7fb49fb1b1cae, reversing
    changes made to 0e12e5890e94e837a1c0b36751c0a8da3c7e6fa7.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    AuthModule, 
-    UsersModule,
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT ?? 3000;
-  await app.listen(port);
-  console.log(`Server running at http://localhost:${port}`);
+  await app.listen(process.env.PORT ?? 3000);
 }
-
-
 bootstrap();
